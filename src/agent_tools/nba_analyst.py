c01a7a17f61e7ca3e0fa93bde148e51b1a63f203
Jordan Smith <jordan.smith@tpx.com>
2024-07-05 12:36:09 -0600

    setting up nba_analyst

---
+++ src/agent_tools/nba_analyst.py
@@ -0,0 +1,13 @@
+def request_data(request: str) -> str:
+    """
+    Request data from the NBA API.
+    :param request: The request to make to the NBA Data Guy.
+    :return: The data from the NBA Data Guy.
+    """
+    pass
+
+
+# Define the function map for the NBA Analyst.
+function_map = {
+    "request_data": request_data
+}
--- src/nba_analyst.py
+++ src/nba_analyst.py
@@ -1,2 +1,27 @@
+from settings import logging
+from db_tools import get_agent_from_db
+from agents import Agent
+from agent_tools.nba_analyst import function_map
+
+
 def nba_analyst(main_thread_id: str, message: str):
-    pass
+
+    # Get the agent from the database
+    db_agent = get_agent_from_db("nba_analyst", "nba")
+
+    # Initialize the agent
+    logging.info("Initializing NBA Analyst.")
+    analyst = Agent(
+        name="nba_analyst",
+        instructions=db_agent["instructions"],
+        model=db_agent["model"],
+        tools=db_agent["tools"],
+        main_thread_id=main_thread_id,
+        function_map=function_map
+    )
+
+    # Add the message to the conversation
+    analyst.add_message(message)
+
+
+
